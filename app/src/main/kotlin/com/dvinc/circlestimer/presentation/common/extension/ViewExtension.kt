0ccff95e33af0da7c24c17dcbbb8a0bf8a67ec6f
d.verentsov <decsent@yandex.ru>
2018-06-28 23:11:46 +0300

    Added few view extensions.

---
+++ app/src/main/kotlin/com/dvinc/circlestimer/presentation/common/extension/ViewExtension.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018 by Denis Verentsov (decsent@yandex.ru)
+ * All rights reserved.
+ */
+
+package com.dvinc.circlestimer.presentation.common.extension
+
+import android.view.View
+
+fun View.toggleGone(visible: Boolean) {
+    visibility = if (visible) View.VISIBLE else View.GONE
+}
+
+fun View.makeGone() {
+    this.visibility = View.GONE
+}
+
+fun View.makeVisible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.makeInvisible() {
+    visibility = View.INVISIBLE
+}
