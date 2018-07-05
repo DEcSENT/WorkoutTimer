5d1854bd5b886df4f0330f3c3066e0854400ded4
d.verentsov <decsent@yandex.ru>
2018-07-05 22:17:50 +0300

    Added view pager with adapter. Added navigation by swipe or by bottom bar. Also added dummy fragment.

---
+++ app/src/main/kotlin/com/dvinc/circlestimer/presentation/ui/main/DummyFragment.kt
@@ -0,0 +1,10 @@
+package com.dvinc.circlestimer.presentation.ui.main
+
+import com.dvinc.circlestimer.R
+import com.dvinc.circlestimer.presentation.ui.base.BaseFragment
+
+class DummyFragment : BaseFragment() {
+
+    override fun getFragmentLayoutId() = R.layout.fragment_dummy
+
+}
