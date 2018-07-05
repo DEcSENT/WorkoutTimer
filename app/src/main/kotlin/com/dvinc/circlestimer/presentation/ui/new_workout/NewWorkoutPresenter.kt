f8d6f98c3fa5c6f68e9e94841ba566d6174085b2
d.verentsov <decsent@yandex.ru>
2018-07-05 22:05:39 +0300

    Added copyright.

--- app/src/main/kotlin/com/dvinc/circlestimer/presentation/ui/new_workout/NewWorkoutPresenter.kt
+++ app/src/main/kotlin/com/dvinc/circlestimer/presentation/ui/new_workout/NewWorkoutPresenter.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2018 by Denis Verentsov (decsent@yandex.ru)
+ * All rights reserved.
+ */
+
 package com.dvinc.circlestimer.presentation.ui.new_workout
 
 import com.dvinc.circlestimer.presentation.ui.base.BasePresenter
