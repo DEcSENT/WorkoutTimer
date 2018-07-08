a5980ea9ff1b4822d469f89682b56bd0ac7e20cf
d.verentsov <decsent@yandex.ru>
2018-07-08 18:35:38 +0300

    Added animation for "Add new workout" button.

--- app/src/main/kotlin/com/dvinc/circlestimer/presentation/ui/workout/WorkoutFragment.kt
+++ app/src/main/kotlin/com/dvinc/circlestimer/presentation/ui/workout/WorkoutFragment.kt
@@ -5,22 +5,29 @@
 
 package com.dvinc.circlestimer.presentation.ui.workout
 
+import android.animation.Animator
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.dvinc.circlestimer.App
 import com.dvinc.circlestimer.R
 import com.dvinc.circlestimer.domain.model.workout.Workout
+import com.dvinc.circlestimer.presentation.common.extension.makeGone
+import com.dvinc.circlestimer.presentation.common.extension.makeVisible
+import com.dvinc.circlestimer.presentation.common.view.SimpleAnimationListener
 import com.dvinc.circlestimer.presentation.model.workout.WorkoutItem
 import com.dvinc.circlestimer.ui.base.BaseFragment
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
 import javax.inject.Inject
 import kotlinx.android.synthetic.main.fragment_workout.fragment_workout_recycler as workoutRecycler
+import kotlinx.android.synthetic.main.fragment_workout.fragment_workout_add_button as addWorkoutButton
 
 class WorkoutFragment : BaseFragment(), WorkoutView {
 
     companion object {
         const val TAG = "WorkoutFragment"
+        private const val ADD_BUTTON_ANIMATION_DURATION = 200L
     }
 
     @Inject
@@ -33,10 +40,7 @@ class WorkoutFragment : BaseFragment(), WorkoutView {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        context?.let {
-            App.get(it).appComponent.inject(this)
-        }
-
+        injectPresenter()
         initWorkoutsList()
         setupSwipeToDelete()
         setupAddButton()
@@ -74,6 +78,12 @@ class WorkoutFragment : BaseFragment(), WorkoutView {
         //TODO: show error
     }
 
+    private fun injectPresenter() {
+        context?.let {
+            App.get(it).appComponent.inject(this)
+        }
+    }
+
     private fun initWorkoutsList() {
         workoutRecycler.adapter = workoutAdapter
     }
@@ -83,6 +93,40 @@ class WorkoutFragment : BaseFragment(), WorkoutView {
     }
 
     private fun setupAddButton() {
-        //TODO: click listener for add button
+        //TODO: call new workout dialog from here
+
+        workoutRecycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                if (dy > 0)
+                    hideAddWorkoutButton()
+                else if (dy < 0){
+                    showAddWorkoutButton()
+                }
+            }
+        })
+    }
+
+    private fun hideAddWorkoutButton() {
+        addWorkoutButton
+                .animate()
+                .alpha(0.0f)
+                .setDuration(ADD_BUTTON_ANIMATION_DURATION)
+                .setListener(object : SimpleAnimationListener() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        addWorkoutButton.makeVisible()
+                    }
+                })
+    }
+
+    private fun showAddWorkoutButton() {
+        addWorkoutButton
+                .animate()
+                .alpha(1.0f)
+                .setDuration(ADD_BUTTON_ANIMATION_DURATION)
+                .setListener(object : SimpleAnimationListener() {
+                    override fun onAnimationStart(animation: Animator?, isReverse: Boolean) {
+                        addWorkoutButton.makeGone()
+                    }
+                })
     }
 }
