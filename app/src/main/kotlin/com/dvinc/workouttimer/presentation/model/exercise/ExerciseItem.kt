882ed6b73d9e94d61ed515d29e4546a3205c20f6
d.verentsov <decsent@yandex.ru>
2018-07-28 01:45:57 +0300

    Added ExerciseItem (WIP). Fixed Exercise item xml.

---
+++ app/src/main/kotlin/com/dvinc/workouttimer/presentation/model/exercise/ExerciseItem.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2018 by Denis Verentsov (decsent@yandex.ru)
+ * All rights reserved.
+ */
+
+package com.dvinc.workouttimer.presentation.model.exercise
+
+import com.dvinc.workouttimer.R
+import com.xwray.groupie.kotlinandroidextensions.Item
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import kotlinx.android.synthetic.main.item_exercise.item_exercise_name as name
+import kotlinx.android.synthetic.main.item_exercise.item_exercise_type as type
+import kotlinx.android.synthetic.main.item_exercise.item_exercise_icon as icon
+import kotlinx.android.synthetic.main.item_exercise.item_exercise_time as time
+
+class ExerciseItem(val exercise: ExerciseUi) : Item() {
+
+    override fun getLayout() = R.layout.item_exercise
+
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        with(viewHolder) {
+            name.text = exercise.name
+            time.text = exercise.time
+            //TODO: finish this
+        }
+    }
+}
+ 
