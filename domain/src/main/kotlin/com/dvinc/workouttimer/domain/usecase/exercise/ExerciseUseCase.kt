e3daff96aaaa10bf8c3ef8431b3d80d8d0cf8f78
d.verentsov <decsent@yandex.ru>
2018-07-29 19:00:35 +0300

    Added ThreadScheduler class for better testing in future. Refactored scheduling flow in use cases. Removed RxExt class.

--- domain/src/main/kotlin/com/dvinc/workouttimer/domain/usecase/exercise/ExerciseUseCase.kt
+++ domain/src/main/kotlin/com/dvinc/workouttimer/domain/usecase/exercise/ExerciseUseCase.kt
@@ -5,19 +5,20 @@
 
 package com.dvinc.workouttimer.domain.usecase.exercise
 
-import com.dvinc.workouttimer.domain.common.extension.getIoToMainTransformerFlowable
+import com.dvinc.workouttimer.domain.common.execution.ThreadScheduler
+import com.dvinc.workouttimer.domain.common.execution.scheduleIoToUi
 import com.dvinc.workouttimer.domain.model.exercise.Exercise
 import com.dvinc.workouttimer.domain.repository.exercise.ExerciseRepository
 import io.reactivex.Flowable
 import javax.inject.Inject
 
 class ExerciseUseCase @Inject constructor(
+        private val scheduler: ThreadScheduler,
         private val exercisesRepository: ExerciseRepository
 ) {
 
     fun obtainExercisesForCurrentWorkout(): Flowable<List<Exercise>> {
         return exercisesRepository.obtainExercises()
-                .compose(getIoToMainTransformerFlowable())
+                .scheduleIoToUi(scheduler)
     }
 }
- 
--- domain/src/main/kotlin/com/dvinc/workouttimer/domain/usecase/workout/WorkoutUseCase.kt
+++ domain/src/main/kotlin/com/dvinc/workouttimer/domain/usecase/workout/WorkoutUseCase.kt
@@ -5,7 +5,8 @@
 
 package com.dvinc.workouttimer.domain.usecase.workout
 
-import com.dvinc.workouttimer.domain.common.extension.getIoToMainTransformerFlowable
+import com.dvinc.workouttimer.domain.common.execution.ThreadScheduler
+import com.dvinc.workouttimer.domain.common.execution.scheduleIoToUi
 import com.dvinc.workouttimer.domain.model.workout.Workout
 import com.dvinc.workouttimer.domain.repository.workout.WorkoutRepository
 import io.reactivex.Completable
@@ -13,17 +14,18 @@ import io.reactivex.Flowable
 import javax.inject.Inject
 
 class WorkoutUseCase @Inject constructor(
+        private val scheduler: ThreadScheduler,
         private val workoutRepository: WorkoutRepository
 ) {
 
     fun obtainWorkouts(): Flowable<List<Workout>>{
         return workoutRepository.obtainWorkouts()
-                .compose(getIoToMainTransformerFlowable())
+                .scheduleIoToUi(scheduler)
     }
 
     fun selectActiveWorkout(workoutId: Int): Completable {
         //TODO: flow for workout updating
         return Completable.complete()
+                .scheduleIoToUi(scheduler)
     }
 }
- 
