3732f9d002ed353092cb6ed31a4438d3c60fa8ce
d.verentsov <decsent@yandex.ru>
2018-07-31 22:56:37 +0300

    Added few database queries. Fixed bug room database by increasing Room version.

--- data/src/main/kotlin/com/dvinc/workouttimer/data/database/dao/ExerciseDao.kt
+++ data/src/main/kotlin/com/dvinc/workouttimer/data/database/dao/ExerciseDao.kt
@@ -7,12 +7,17 @@ package com.dvinc.workouttimer.data.database.dao
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
 import com.dvinc.workouttimer.data.model.exercise.ExerciseEntity
+import io.reactivex.Flowable
 
 @Dao
 abstract class ExerciseDao {
 
     @Insert
     abstract fun addExercises(exercises: List<ExerciseEntity>)
+
+    @Query("SELECT * FROM exercise WHERE workout_id = :id")
+    abstract fun getExercisesByWorkoutId(id: Int): Flowable<List<ExerciseEntity>>
 }
  
--- data/src/main/kotlin/com/dvinc/workouttimer/data/database/dao/WorkoutDao.kt
+++ data/src/main/kotlin/com/dvinc/workouttimer/data/database/dao/WorkoutDao.kt
@@ -7,12 +7,21 @@ package com.dvinc.workouttimer.data.database.dao
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
 import com.dvinc.workouttimer.data.model.workout.WorkoutEntity
+import io.reactivex.Flowable
+import io.reactivex.Single
 
 @Dao
 abstract class WorkoutDao {
 
     @Insert
     abstract fun addWorkout(workout: WorkoutEntity)
+
+    @Query("SELECT * FROM workout")
+    abstract fun getWorkouts(): Flowable<List<WorkoutEntity>>
+
+    @Query("SELECT * FROM workout WHERE id = :workoutId")
+    abstract fun getWorkoutById(workoutId: Int): Single<WorkoutEntity>
 }
  
