2e432cac094637b42609dd009aa28aa97baac7b6
d.verentsov <decsent@yandex.ru>
2018-10-07 20:02:02 +0300

    Added ability to expand and collapse workout item on click event.

--- app/src/main/kotlin/com/dvinc/workouttimer/presentation/common/adapter/item/workout/WorkoutItem.kt
+++ app/src/main/kotlin/com/dvinc/workouttimer/presentation/common/adapter/item/workout/WorkoutItem.kt
@@ -15,9 +15,11 @@ import kotlinx.android.synthetic.main.item_workout.item_workout_description as w
 import kotlinx.android.synthetic.main.item_workout.item_workout_total_exercises_count as exercisesCount
 import kotlinx.android.synthetic.main.item_workout.item_workout_exercises_total_time as totalTime
 import kotlinx.android.synthetic.main.item_workout.item_workout_active as activeLabel
+import kotlinx.android.synthetic.main.item_workout.item_workout_buttons_group as workoutControlButtonsGroup
 
 class WorkoutItem constructor(
-        val workout: WorkoutUi
+        val workout: WorkoutUi,
+        private var isExpanded: Boolean = false
 ) : Item() {
 
     override fun getLayout() = R.layout.item_workout
@@ -27,8 +29,15 @@ class WorkoutItem constructor(
             workoutName.text = workout.name
             workoutDesc.text = workout.description
             exercisesCount.text = workout.exerciseCount.toString()
-            totalTime.text = workout.exerciseTotalTime.toString()
+            totalTime.text = workout.exerciseTotalTime
             activeLabel.toggleGone(workout.isActive)
+
+            workoutControlButtonsGroup.toggleGone(isExpanded)
+
+            itemView.setOnClickListener {
+                isExpanded = !isExpanded
+                notifyChanged()
+            }
         }
     }
 }
