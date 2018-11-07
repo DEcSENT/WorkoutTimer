4ee696cfe5e38f2c5473cd540ce684dfbf781f5b
d.verentsov <decsent@yandex.ru>
2018-11-07 22:04:43 +0300

    Removed old BasePresenter, BaseView and Assert classes.

--- app/src/main/kotlin/com/dvinc/workouttimer/presentation/ui/base/BaseMvpView.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2018 by Denis Verentsov (decsent@yandex.ru)
- * All rights reserved.
- */
-
-package com.dvinc.workouttimer.presentation.ui.base
-
-@Deprecated("Delete after migration to viewModels")
-interface BaseMvpView
