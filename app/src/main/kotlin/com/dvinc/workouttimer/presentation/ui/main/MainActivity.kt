da0ff515ae9f40b2736fb56353aa3447d8e640e2
d.verentsov <decsent@yandex.ru>
2018-12-25 22:09:08 +0300

    Added basic navigation on main screen (WIP).

--- app/src/main/kotlin/com/dvinc/workouttimer/presentation/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/dvinc/workouttimer/presentation/ui/main/MainActivity.kt
@@ -7,7 +7,6 @@ package com.dvinc.workouttimer.presentation.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
 import com.dvinc.workouttimer.R
 import kotlinx.android.synthetic.main.activity_main.activity_main_bottom_navigation as bottomNavigation
@@ -20,9 +19,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setupBottomNavigation()
-
-        //TODO: Replace DummyFragments
-        replaceFragment(DummyFragment(), "Replace me")
     }
 
     private fun setupBottomNavigation() {
@@ -30,7 +26,8 @@ class MainActivity : AppCompatActivity() {
             //TODO: Prevent fragment duplicate
             when (it.itemId) {
                 R.id.action_timer -> {
-                    replaceFragment(DummyFragment(), "Replace me")
+                    findNavController(this@MainActivity, R.id.navigation_host_fragment)
+                            .navigate(R.id.action_global_timerFragment)
                     true
                 }
                 R.id.action_exercise -> {
@@ -40,34 +37,18 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
                 R.id.action_workout -> {
-                    //replaceFragment(WorkoutFragment(), WorkoutFragment.TAG)
                     findNavController(this@MainActivity, R.id.navigation_host_fragment)
                             .navigate(R.id.action_global_workoutFragment)
                     true
                 }
                 R.id.action_settings -> {
-                    replaceFragment(DummyFragment(), "Replace me 2")
+                    findNavController(this@MainActivity, R.id.navigation_host_fragment)
+                            .navigate(R.id.action_global_settingsFragment)
                     true
                 }
                 else -> true
 
             }
-        }
-    }
-
-    private fun replaceFragment(fragment: Fragment, tag: String) {
-        with(this.supportFragmentManager) {
-            beginTransaction()
-                    .apply {
-                        setCustomAnimations(
-                                R.anim.animation_fade_in,
-                                R.anim.animation_fade_out,
-                                R.anim.animation_fade_in,
-                                R.anim.animation_fade_out
-                        )
-                    }
-                    .replace(R.id.activity_main_fragment_container, fragment, tag)
-                    .commit()
         }
     }
 }
