94a7fb2ef716e6fa100eb25f1caacfe7b7b47781
d.verentsov <decsent@yandex.ru>
2018-12-25 22:04:43 +0300

    Added empty Timer and Settings fragments.

---
+++ app/src/main/kotlin/com/dvinc/workouttimer/presentation/ui/settings/SettingsFragment.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018 by Denis Verentsov (decsent@yandex.ru)
+ * All rights reserved.
+ */
+
+package com.dvinc.workouttimer.presentation.ui.settings
+
+import com.dvinc.workouttimer.R
+import com.dvinc.workouttimer.presentation.ui.base.BaseFragment
+
+/*
+ * This fragment is empty, for now.
+ */
+class SettingsFragment : BaseFragment() {
+
+    override fun getFragmentLayoutId() = R.layout.fragment_settings
+
+    override fun injectDependencies() {
+    }
+
+    override fun clearDependencies() {
+    }
+
+    override fun initViewModel() {
+    }
+}
---
+++ app/src/main/kotlin/com/dvinc/workouttimer/presentation/ui/timer/TimerFragment.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018 by Denis Verentsov (decsent@yandex.ru)
+ * All rights reserved.
+ */
+
+package com.dvinc.workouttimer.presentation.ui.timer
+
+import com.dvinc.workouttimer.R
+import com.dvinc.workouttimer.presentation.ui.base.BaseFragment
+
+/*
+ * This fragment is empty, for now.
+ */
+class TimerFragment : BaseFragment() {
+
+    override fun getFragmentLayoutId() = R.layout.fragment_timer
+
+    override fun injectDependencies() {
+    }
+
+    override fun clearDependencies() {
+    }
+
+    override fun initViewModel() {
+    }
+}
