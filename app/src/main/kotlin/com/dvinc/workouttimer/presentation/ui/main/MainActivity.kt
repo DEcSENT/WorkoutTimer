094792e61583b549ac4f42a1e688ac792aea6828
d.verentsov <decsent@yandex.ru>
2018-12-26 22:43:57 +0300

    Added basic navigation on main screen.

--- app/src/main/kotlin/com/dvinc/workouttimer/presentation/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/dvinc/workouttimer/presentation/ui/main/MainActivity.kt
@@ -7,48 +7,21 @@ package com.dvinc.workouttimer.presentation.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.dvinc.workouttimer.R
 import kotlinx.android.synthetic.main.activity_main.activity_main_bottom_navigation as bottomNavigation
-import kotlinx.android.synthetic.main.activity_main.activity_main_fragment_container as fragmentContainer
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setupBottomNavigation()
     }
 
     private fun setupBottomNavigation() {
-        bottomNavigation.setOnNavigationItemSelectedListener {
-            //TODO: Prevent fragment duplicate
-            when (it.itemId) {
-                R.id.action_timer -> {
-                    findNavController(this@MainActivity, R.id.navigation_host_fragment)
-                            .navigate(R.id.action_global_timerFragment)
-                    true
-                }
-                R.id.action_exercise -> {
-                    findNavController(this@MainActivity, R.id.navigation_host_fragment)
-                            .navigate(R.id.action_global_exerciseFragment)
-                    //replaceFragment(ExerciseFragment(), ExerciseFragment.TAG)
-                    true
-                }
-                R.id.action_workout -> {
-                    findNavController(this@MainActivity, R.id.navigation_host_fragment)
-                            .navigate(R.id.action_global_workoutFragment)
-                    true
-                }
-                R.id.action_settings -> {
-                    findNavController(this@MainActivity, R.id.navigation_host_fragment)
-                            .navigate(R.id.action_global_settingsFragment)
-                    true
-                }
-                else -> true
-
-            }
-        }
+        val navController = findNavController(R.id.activity_main_navigation_host_fragment)
+        bottomNavigation.setupWithNavController(navController)
     }
 }
