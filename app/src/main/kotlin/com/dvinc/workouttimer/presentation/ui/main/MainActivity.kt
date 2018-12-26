d959b74df93176bc409b7e6d1a00c51eb1e9ff15
d.verentsov <decsent@yandex.ru>
2018-12-26 22:53:50 +0300

    Removed bad navigation from android arch component. Restored classical and simple navigation.

--- app/src/main/kotlin/com/dvinc/workouttimer/presentation/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/dvinc/workouttimer/presentation/ui/main/MainActivity.kt
@@ -7,9 +7,12 @@ package com.dvinc.workouttimer.presentation.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
 import com.dvinc.workouttimer.R
+import com.dvinc.workouttimer.presentation.ui.exercise.ExerciseFragment
+import com.dvinc.workouttimer.presentation.ui.settings.SettingsFragment
+import com.dvinc.workouttimer.presentation.ui.timer.TimerFragment
+import com.dvinc.workouttimer.presentation.ui.workout.WorkoutFragment
 import kotlinx.android.synthetic.main.activity_main.activity_main_bottom_navigation as bottomNavigation
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +21,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupBottomNavigation()
+
+        //Initial fragment
+        replaceFragment(TimerFragment(), "Timer")
     }
 
     private fun setupBottomNavigation() {
-        val navController = findNavController(R.id.activity_main_navigation_host_fragment)
-        bottomNavigation.setupWithNavController(navController)
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            //TODO: Prevent fragment duplicate
+            when (it.itemId) {
+                R.id.action_timer -> {
+                    replaceFragment(TimerFragment(), "Timer")
+                    true
+                }
+                R.id.action_exercise -> {
+                    replaceFragment(ExerciseFragment(), ExerciseFragment.TAG)
+                    true
+                }
+                R.id.action_workout -> {
+                    replaceFragment(WorkoutFragment(), WorkoutFragment.TAG)
+                    true
+                }
+                R.id.action_settings -> {
+                    replaceFragment(SettingsFragment(), "Settings")
+                    true
+                }
+                else -> true
+
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment, tag: String) {
+        with(this.supportFragmentManager) {
+            beginTransaction()
+                    .apply {
+                        setCustomAnimations(
+                                R.anim.animation_fade_in,
+                                R.anim.animation_fade_out,
+                                R.anim.animation_fade_in,
+                                R.anim.animation_fade_out
+                        )
+                    }
+                    .replace(R.id.activity_main_fragment_container, fragment, tag)
+                    .commit()
+        }
     }
 }
